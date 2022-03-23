b3b98d4b45c98aa07fc8fe59805c3e0d15ef52dd
kat055 <gacusankatherine0514@gmail.com>
2022-03-22 23:30:16 -0700

    first

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,38 +9,34 @@
   </form>
 </div>
 <div class="rectangle">
-  <svg width="400" height="600">
-    <rect x="60" y="20" rx="30" ry="30" width="200" height="300" style="background: linear-gradient(to bottom right, #ff3300 50%, #ff6600 100%);"/>
-  </svg>
-  <svg width="400" height="600">
-    <rect x="60" y="20" rx="30" ry="30" width="200" height="300" style="background: linear-gradient(to top left, #0000cc 2%, #33cccc 100%);"/>
-  </svg>
-  <svg width="400" height="600">
-    <rect x="60" y="20" rx="30" ry="30" width="200" height="300" style="background: linear-gradient(to bottom right, #00ff00 0%, #ffff00 100%);"/>
-  </svg>
-  <svg width="400" height="600">
-    <rect x="60" y="20" rx="30" ry="30" width="200" height="300" style="background: linear-gradient(to top left, #9900cc 0%, #ff0066 103%);"/>
-  </svg>
+    <div class="rect" style="background: linear-gradient(to bottom right, #ff3300 50%, #ff6600 100%);"></div>
+    <div class="rect" style="background: linear-gradient(to top left, #0000cc 2%, #33cccc 100%);"></div>
+    <div class="rect" style="background: linear-gradient(to bottom right, #00ff00 0%, #ffff00 100%);"></div>
+    <div class="rect" style="background: linear-gradient(to top left, #9900cc 0%, #ff0066 103%);"></div>
 </div>
-<div>
-  <table>
-    <tr>
-      <th>Trending</th>
-      <th>Most Searched</th>
-    </tr>
-    <tr>
-      <td>MSFT</td>
-      <td>BRB.A</td>
-    </tr>
-    <tr>
-      <td>AAPL</td>
-      <td>KO</td>
-    </tr>
-    <tr>
-      <td>SBUX</td>
-      <td>TSL;A</td>
-    </tr>
-  </table>
+<div class="list">
+  <div class="list1" style="padding-right: 6in;">
+<div class="listb">
+  <div class="listh">Trending</div>
+  <div class="listb">
+  <ul style="list-style-type: none;">
+    <li>MSFT</li>
+    <li>AAPPL</li>
+    <li>SBUX</li>
+  </ul>
+  </div>
+  </div>
+</div>
+<div class="listb">
+  <div class="listh">Most Searched</div>
+  <div class="listb">
+  <ul style="list-style-type: none;">
+    <li>BRB.A</li>
+    <li>KO</li>
+    <li>TSL;A</li>
+  </ul>
+  </div>
+  </div>
 </div>
 </template>
 
@@ -66,11 +62,28 @@
 .rectangle{
   display: flex;
   flex-direction: row;
-  gap: none;
+  gap: 50px;
+  padding-left: 90px;
+  padding-top: 50px;
+  padding-bottom: 50px;
 }
-.table th{
+.rect{
+  height: 400px;
+  width: 250px;
+  border-radius: 15px;
+}
+.list{
+  display: flex;
+  flex-direction: row;
   font-family: roboto;
-  font-weight: bold;
+  padding-top: 1cm;
+  padding-left: 2.5cm;
+}
+.listh{
+  font-weight: bolder;
   font-size: xx-large;
+}
+.listb{
+  font-size: larger;
 }
 </style>
